7478fdfb565c247d10531430c382933175e9b5c2
poisnada <horberlan@gmail.com>
2024-10-04 18:29:11 -0300

    feat: set router query on-redirect to child path

--- lib/router_controller_web/controllers/redirect_controller.ex
+++ lib/router_controller_web/controllers/redirect_controller.ex
@@ -1,14 +1,14 @@
 defmodule RouterControllerWeb.RedirectController do
   use RouterControllerWeb, :controller
 
-  def index(conn, _params) do
+  def index(conn, %{"user_name" => user_name}) do
     base_url = Application.get_env(:router_controller_web, :base_url)
 
-    data = %{user_data: "value"}
-
     conn
-    |> put_session(:user_data, "value")
-    |> redirect(external: "#{base_url}?#{URI.encode_query(data)}")
+    |> put_session(:user_data, "#{user_name}")
+    |> redirect(external:  "#{base_url}/#{user_name}")
+    conn
+    |> send_resp(400, "Parâmetro 'user_data' não foi fornecido.")
   end
 
   def page(conn, _params) do
