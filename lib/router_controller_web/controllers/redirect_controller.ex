d3e91e84decade74bdd85563da705a17bfbee08c
poisnada <horberlan@gmail.com>
2024-10-04 14:17:43 -0300

    chore: set user session on-redirect handler

--- lib/router_controller_web/controllers/redirect_controller.ex
+++ lib/router_controller_web/controllers/redirect_controller.ex
@@ -3,12 +3,22 @@ defmodule RouterControllerWeb.RedirectController do
 
   def index(conn, _params) do
     base_url = Application.get_env(:router_controller_web, :base_url, "http://localhost:3000")
-    redirect(conn, external: "#{base_url}?data=#{URI.encode_query(%{key: "value"})}")
+
+    data = %{user_data: "value"}
+
+    conn
+    |> put_session(:user_data, "value")
+    |> redirect(external: "#{base_url}?#{URI.encode_query(data)}")
   end
 
   def page(conn, _params) do
-    # If you want a different base_url for this function, you can set another environment variable
-    base_url = Application.get_env(:router_controller_web, :base_url_for_page, "http://localhost:5173")
-    redirect(conn, external: "#{base_url}?data=#{URI.encode_query(%{key: "value"})}")
+    base_url_for_page =
+      Application.get_env(:router_controller_web, :base_url_for_page, "http://localhost:5173")
+
+    data = %{user_data: "value"}
+
+    conn
+    |> put_session(:user_data, "value")
+    |> redirect(external: "#{base_url_for_page}?#{URI.encode_query(data)}")
   end
 end
