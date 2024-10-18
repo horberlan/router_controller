289837e8ff068475786e751f905207c292028043
poisnada <horberlan@gmail.com>
2024-10-17 22:41:54 -0300

    feat: add access counter query param

--- lib/router_controller_web/controllers/redirect_controller.ex
+++ lib/router_controller_web/controllers/redirect_controller.ex
@@ -10,8 +10,8 @@ defmodule RouterControllerWeb.RedirectController do
       |> json(%{error: "Missing 'user_name' parameter"})
     else
       conn
-      |> put_session(:user_data, user_name)
-      |> redirect(external: "#{base_url}/#{user_name}")
+      |> increment_access_counter()
+      |> redirect_to_user_page(base_url, user_name)
     end
   end
 
@@ -22,13 +22,26 @@ defmodule RouterControllerWeb.RedirectController do
   end
 
   def page(conn, _params) do
-    base_url_for_page =
-      Application.get_env(:router_controller_web, :base_url_for_page)
-
-    data = %{user_data: "value"}
+    base_url_for_page = Application.get_env(:router_controller_web, :base_url_for_page)
 
     conn
     |> put_session(:user_data, "value")
-    |> redirect(external: "#{base_url_for_page}?#{URI.encode_query(data)}")
+    |> redirect_to_page(base_url_for_page)
+  end
+
+  defp increment_access_counter(conn) do
+    current_counter = get_session(conn, :access_counter) || 0
+
+    conn
+    |> put_session(:access_counter, current_counter + 1)
+  end
+
+  defp redirect_to_user_page(conn, base_url, user_name) do
+    access_counter = get_session(conn, :access_counter)
+    redirect(conn, external: "#{base_url}/#{user_name}?access=#{access_counter}")
+  end
+
+  defp redirect_to_page(conn, base_url_for_page) do
+    redirect(conn, external: "#{base_url_for_page}?#{URI.encode_query(user_data: "value")}")
   end
 end
